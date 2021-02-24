33cc52808d90d7ec8842ab27d39f0198a17e291c
Karl Yann Fouezeu Zeufack <karlydiamond214@gmail.com>
2021-02-24 01:32:16 +0100

    Initial commit

---
+++ readerbench/src/main/java/i5/las2peer/services/readerbenchService/ReaderbenchService.java
@@ -0,0 +1,173 @@
+package i5.las2peer.services.readerbenchService;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import i5.las2peer.api.Context;
+import i5.las2peer.api.security.UserAgent;
+import i5.las2peer.restMapper.RESTService;
+import i5.las2peer.restMapper.annotations.ServicePath;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Contact;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.License;
+import io.swagger.annotations.SwaggerDefinition;
+import net.minidev.json.JSONObject;
+
+// TODO Describe your own service
+/**
+ * las2peer-Template-Service
+ * 
+ * This is a template for a very basic las2peer service that uses the las2peer WebConnector for RESTful access to it.
+ * 
+ * Note: If you plan on using Swagger you should adapt the information below in the SwaggerDefinition annotation to suit
+ * your project. If you do not intend to provide a Swagger documentation of your service API, the entire Api and
+ * SwaggerDefinition annotation should be removed.
+ * 
+ */
+// TODO Adjust the following configuration
+@Api
+@SwaggerDefinition(
+		info = @Info(
+				title = "las2peer Template Service",
+				version = "1.0.0",
+				description = "A las2peer Template Service for demonstration purposes.",
+				termsOfService = "http://your-terms-of-service-url.com",
+				contact = @Contact(
+						name = "John Doe",
+						url = "provider.com",
+						email = "john.doe@provider.com"),
+				license = @License(
+						name = "your software license name",
+						url = "http://your-software-license-url.com")))
+@ServicePath("/template")
+// TODO Your own service class
+public class ReaderbenchService extends RESTService {
+
+	/**
+	 * Template of a get function.
+	 * 
+	 * @return Returns an HTTP response with the username as string content.
+	 */
+	@GET
+	@Path("/get")
+	@Produces(MediaType.TEXT_PLAIN)
+	@ApiOperation(
+			value = "REPLACE THIS WITH AN APPROPRIATE FUNCTION NAME",
+			notes = "REPLACE THIS WITH YOUR NOTES TO THE FUNCTION")
+	@ApiResponses(
+			value = { @ApiResponse(
+					code = HttpURLConnection.HTTP_OK,
+					message = "REPLACE THIS WITH YOUR OK MESSAGE") })
+	public Response getTemplate() {
+		UserAgent userAgent = (UserAgent) Context.getCurrent().getMainAgent();
+		String name = userAgent.getLoginName();
+		return Response.ok().entity(name).build();
+	}
+
+	/**
+	 * Template of a post function.
+	 * 
+	 * @param myInput The post input the user will provide.
+	 * @return Returns an HTTP response with plain text string content derived from the path input param.
+	 */
+	@POST
+	@Path("/post/{input}")
+	@Produces(MediaType.TEXT_PLAIN)
+	@ApiResponses(
+			value = { @ApiResponse(
+					code = HttpURLConnection.HTTP_OK,
+					message = "REPLACE THIS WITH YOUR OK MESSAGE") })
+	@ApiOperation(
+			value = "REPLACE THIS WITH AN APPROPRIATE FUNCTION NAME",
+			notes = "Example method that returns a phrase containing the received input.")
+	public Response postTemplate(@PathParam("input") String myInput) {
+		String returnString = "";
+		returnString += "Input " + myInput;
+		return Response.ok().entity(returnString).build();
+	}
+
+	// TODO your own service methods, e. g. for RMI
+	
+	@GET
+	@Path("/getRbStatus")
+	@Produces(MediaType.TEXT_PLAIN)
+	@ApiOperation(
+			value = "REPLACE THIS WITH AN APPROPRIATE FUNCTION NAME",
+			notes = "REPLACE THIS WITH YOUR NOTES TO THE FUNCTION")
+	@ApiResponses(
+			value = { @ApiResponse(
+					code = HttpURLConnection.HTTP_OK,
+					message = "REPLACE THIS WITH YOUR OK MESSAGE") })
+	public Response getRbStatus() {
+		try {
+			//Creating a HttpClient object
+			CloseableHttpClient httpclient = HttpClients.createDefault();
+			//Creating a HttpGet object
+		    HttpGet httpget = new HttpGet("http://localhost:6006/api/v1/isalive ");
+		
+		    //Printing the method used
+		    System.out.println("Request Type: "+httpget.getMethod());
+		
+		   //Executing the Get request
+		   HttpResponse response = httpclient.execute(httpget);
+		   return Response.ok().entity(response.getEntity().getContent().toString()).build();
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+		}
+	}
+	
+	@POST
+	@Path("/textual-complexity")
+	@Produces(MediaType.TEXT_PLAIN)
+	@ApiResponses(
+			value = { @ApiResponse(
+					code = HttpURLConnection.HTTP_OK,
+					message = "REPLACE THIS WITH YOUR OK MESSAGE") })
+	@ApiOperation(
+			value = "REPLACE THIS WITH AN APPROPRIATE FUNCTION NAME",
+			notes = "Example method that returns a phrase containing the received input.")
+	public Response textualComplexityPost(String body) {
+		JSONObject j = new JSONObject();
+		j.put("language", "en");
+		j.put("text", body);
+		try {
+			StringEntity entity = new StringEntity(j.toString());
+			HttpClient httpClient = HttpClientBuilder.create().build();
+	        HttpPost request = new HttpPost("http://localhost:6006/api/v1/register");
+	        request.setEntity(entity);
+	        HttpResponse response = httpClient.execute(request);
+			return Response.ok().entity(response.getEntity().getContent().toString()).build();
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
+		}
+	}
+
+}
